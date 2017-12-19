9d4dc83d48a5fe519786ec42631ff5d9444ebd12
Mr. Punk <34318752+thepunkprofessor@users.noreply.github.com>
2017-12-19 18:48:10 -0500

    Updated for new strategy

--- client/app.js
+++ client/app.js
@@ -1,6 +1,8 @@
 // This is the code for getting your messages to the websockets server so the bot can tweet
 // Also includes control over modals using the vex library
 // And sets the localstorage so that every user can see how many times they've individually tweeted
+// All references to bot-server.js are from my Githbub
+// Check it out at https://github.com/thepunkprofessor/net-neutrality-twitter-bot to learn/clone 
 
 // This sets the vex theme. See the vex documentation to see what other options are available. They're on github
 vex.defaultOptions.className = 'vex-theme-default';
@@ -30,7 +32,6 @@ userCount.textContent = "Your Count: " + numTimes.toString(10);
 // Feel free to connect to it, but make sure to set up your own so that we can get 
 // as many people tweeting as possible!
 // See the bot-server.js file for how to handle the server side
-// Also make sure to put "wss" instead of "http"!
 let socket = new WebSocket("wss://hickory-area.glitch.me/");
 
 // Console log any errors and let the user know with a nice modal that the connection hasn't occurred.
@@ -43,42 +44,36 @@ socket.onerror = function(error) {
 };
 
 // This establishes the connection to the server, letting the server know you wish to be subscribed 
-// to any new data. This connection ends when the user leaves the site and  
+// to any new data. This connection ends when the user leaves the site amd  
 // reopened when they revisit or refresh
-// It is empty beacuse sending a message here will complicate handling messages on the server
-// If you need to see when people are connected, look in the bot-server.js file
-// and find where the console log occurs for "connection" events and you can change your logging there
 socket.addEventListener("open", function (event) {
 
 });
 
 // This is what will update the HTML whenever any user sends data to the server
 // This is where the real-time functionality of the app comes in
-// The "message" argument here just lets the server know you're sending data so it can respond 
+// The message here just lets the server know you're sending data so it can respond 
 // and update the count for everyone
-// This does not send any data to the server. That comes next
 socket.addEventListener("message", function (event) {
     totalCount.innerHTML = "Total Count: " + event.data;
 });
 
-// Here the message is dispatched. This occurs when the user clicks on your Tweet button
-// The socket sends a nice message to your server letting you know that your users are steadfast in their 
+// Here the message is dispatched. This occurs when the user clicks on the Tweet button
+// The socket sends a nice message to my server letting me know that you are steadfast in your 
 // dedication to the cause
-// and allows for the server to now emit its data back so everyone can see their commitment
-// It also updates the localstorage to show their own personal contribution
+// and allows for the server to now emit it's data back so everyone can see your commitment
+// It also updates the localstorage to show your own personal contribution
 // See the bot-server.js file for more info on how this gets handled server-side
 fight.addEventListener("click", () => {
   socket.send("I Stand For Net Neutrality");
 
-    // This is only for localstorage. This doens't send any incremented value to the server
     numTimes++;
     localStorage.setItem("user-tweet", (numTimes).toString(10))
 
-    // Update your local count in the HTML
     userCount.textContent = "Your Count: " + numTimes.toString(10);
     
     // This shows a modal alert that gives feedback to the user
-    // Replace my twitter account's url in the message with your own if you decide
+    // Replace my twitter account's url with your own if you decide
     // to set up your own twitter bot
     vex.dialog.alert({
         message: "Tweet Sent! Thanks For Taking Part In The Fight To Protect Net Neutrality. Click Again and Fight Some More! Then Check Out My Twitter To See Our Progress: https://twitter.com/punk_professor?lang=en"
@@ -89,6 +84,6 @@ fight.addEventListener("click", () => {
 // This is very important so your user actually understands what the app is doing
 info.addEventListener("click", () => {
     vex.dialog.alert({
-        message: 'I built a Twitter Bot that tweets "X people and I say \'Fuck Ajit Pai\'. Retweet if you do too #netneutrality #FightForFreeInternet https://t.co/KE2wRbq2LC" with a link to sign a petition to save Net Neutrality on change.org. The X here is you and all people who think like you and I do. You will find the total people that have helped fight the cause, as well as your specific contribution below. Net Neutrality is no joke, I strongly urge you to help protect it in any way you can. I have also included a link to my Github where you can find the server code to make your own bot and spread the word. Let\'s get a swarm going!'
+        message: 'I built a Twitter Bot that tweets "X people and I say "Fuck Ajit Pai". We stand for #netneutrality Retweet if you do too! goo.gl/QvSoiY`" with a link to a site dedicated to getting people in touch with their government representatives to show their dissent for the new regulations. The X here is you and all people who think like you and I do. You will find the total people that have helped fight the cause, as well as your specific contribution below. Net Neutrality is no joke, I strongly urge you to help protect it in any way you can. I have also included a link to my Github where you can find the server code to make your own bot and spread the word. Let\'s get a swarm going!'
     });
 });
